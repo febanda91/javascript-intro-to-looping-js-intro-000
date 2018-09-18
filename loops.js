d9c8183966dbade1b3547cb9cb81bca3f539b355
Francisco Ezeqiel Banda <febanda91@gmail.com>
2018-09-18 22:27:45 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -17,3 +17,12 @@ function whileLoop(n) {
   }
   return 'done';
 }
+
+
+function doWhileLoop(array) {
+    incrementVariable();
+do {
+    array.pop();
+} while (array.lenght > 0 && incrementVariable();
+}
+return array 
