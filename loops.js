d836c9bc14caac4cc97e902071a89c4eabf0c1af
Francisco Ezeqiel Banda <febanda91@gmail.com>
2018-09-18 22:07:30 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,11 +1,28 @@
-
 function forLoop(array) {
 for (let i = 0; i < 25; i++) {
-  if (i = 1) {
-    array.push`I am ${i} strange loop.`)
+  if (i === 1) {
+    array.push(`I am ${i} strange loop.`);
   } else {
-    array.push`I am ${i} strange loops.`)
+    array.push(`I am ${i} strange loops.`);
   }
  }
  return array
+}
+
+
+
+function whileLoop(n) {
+  while ( n > 0) {
+    console.log(--n)
+  }
+  return 'done';
+}
+
+
+function doWhileLoop(array) {
+}
+
+do {
+  array.pull(--n);
+} while (array.length > 0 && incrementVariable());
 }
